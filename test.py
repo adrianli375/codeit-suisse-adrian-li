be12126602ef86a5cecb898e258673337c49d6e9
Adrian Li <adrianli375@Adrians-MacBook-Air-3.local>
2022-09-15 21:22:14 +0800

    changes

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ data = {'stream': ["00:06,A,1,5.6",
                     "00:04,A,1,5.6"], 
         'quantityBlock': 5}
 headers = {'Content-type': 'application/json'}
-request = requests.post(# 'https://codeit-suisse-adrian-li.herokuapp.com/square', 
+request = requests.post(
+    # 'https://codeit-suisse-adrian-li.herokuapp.com/square', 
     'https://codeit-suisse-adrian-li.herokuapp.com/tickerStreamPart2', 
                         json=data, headers=headers)
 print(request.text)
